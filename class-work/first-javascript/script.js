795dbe4ea01a4e205e188367a419f2cae0c7df0c
Kelly <kabrennan23@yahoo.com>
2018-10-29 18:20:59 -0400

    first javascript exercise into class-work

---
+++ class-work/first-javascript/script.js
@@ -0,0 +1,6 @@
+var h1 = document.querySelector('h1')
+
+document.querySelector('h1').style.color = "red"
+document.querySelector('h1').style.backgroundColor = "pink"
+document.getElementById("title").style.color = 'red';
+document.getElementById('text-chunk').style.color = 'red'
